6b059c314838556fcc15d4eedb98c16581b751d9
voyagec2 <402806091@qq.com>
2015-10-12 18:19:36 +0800

    Update MarkDownParser.java

--- WikiParser/src/com/sjtu/parser/MarkDownParser.java
+++ WikiParser/src/com/sjtu/parser/MarkDownParser.java
@@ -15,33 +15,42 @@ public class MarkDownParser {
     	while (txt.indexOf("}}")>0) {    // InfoBox  is  in {{ and }} and have sign "Infobox"
     		int end = txt.indexOf("}}");
     		int start = end;
-    		while ((!txt.substring(start,start+2).equals("{{")) && start>=0)	start--; 
+    		while ((!txt.substring(start,start+2).equals("{{")) && start>0)	start--;
+    		if (start ==0 && !txt.substring(start,start+2).equals("{{")) { // {{ miss
+    			txt =txt.substring(0, end)+txt.substring(end+2,txt.length());
+    			continue;			
+    		}
     		tmp = txt.substring(start+2,end);
-    		if (end-start<7) { 
+    		if (tmp.length()<8) { 
     			   txt = txt.substring(0,start)+txt.substring(start+2,end)+txt.substring(end+2,txt.length());	
     		}
     		else
     		if (tmp.substring(0,7).equals("Infobox"))  {    // check
-    			int p = tmp.indexOf("|");
-    			tmp = tmp.substring(p+1,tmp.length());
-    			while (tmp.indexOf("=")>0) {   // Infobox  is split by | and  =
-    				    p = tmp.indexOf("=");
-    					Key = tmp.substring(0,p);
-    					tmp = tmp.substring(p+1,tmp.length());
-    					p = tmp.indexOf("=");
-    					if (p<0) {
-    						Value = getCleanTxt(tmp);
-    					}
-    					else {
-    						while ((!tmp.substring(p,p+1).equals("|")) && p>=0) p--;
-    						Value = getCleanTxt(tmp.substring(0, p));
-    						tmp = tmp.substring(p+1,tmp.length());
-    					}
+    			 int p = tmp.indexOf("\n");
+    			 int q;
+    			 tmp = tmp.substring(p+1,tmp.length());
+    			while (tmp.indexOf("\n")>0) {   // Infobox  is split by \n and  |
+    				   p= tmp.indexOf("\n"); 
+    			
+    				   tmp2 = tmp.substring(0,p);
+    				   tmp = tmp.substring(p+1,tmp.length());
+    				   if (tmp.length()==0) break;
+    				   while (tmp.substring(0,1).equals("*"))  {// not another infobox  
+    					   p = tmp.indexOf("\n"); 
+    					   if (p<0)  tmp2 = tmp2 + tmp;
+    					   else  tmp2 = tmp2 + tmp.substring(0,p);
+    					   tmp = tmp.substring(p+1,tmp.length());
+    					   if (tmp.length()==0) break;
+    				   }
+    				    p = tmp2.indexOf("=");
+    				    q = tmp2.indexOf("|");
+    				    if (p<0 || q<0 || q>p) continue;
+    					Key = tmp2.substring(q+1,p);
+    					Value = getCleanTxt(tmp2.substring(p+1,tmp2.length()));
     					
     					if (!Key.equals("")) Infobox = Infobox + "[" + Key + "="+ Value + "]";
-    					System.out.println("Key:"+Key);
-    		    		System.out.println("Value:"+Value);
     			}
+    			
     			txt = txt.substring(0,start)+txt.substring(end+2,txt.length());
     		}
     		else 
@@ -60,18 +69,17 @@ public class MarkDownParser {
     	while (txt.indexOf("]]")>0) {    // Category's sign is [[ and ]] and have :
     		int end = txt.indexOf("]]");
     		int start = end;
-    		while ((!txt.substring(start,start+2).equals("[[")) && start>=0)	start--; 
+    		while ((!txt.substring(start,start+2).equals("[[")) && start>0)	start--;
+    		if (start ==0 && !txt.substring(start,start+2).equals("[[")) { // [[ miss
+    			txt =txt.substring(0, end)+txt.substring(end+2,txt.length());
+    			continue;			
+    		}
     		tmp = txt.substring(start+2,end);
     		if (tmp.indexOf(":")>0)  {    // [[ : ]] is category
     			int p = tmp.indexOf(":");
     			tmp2 = tmp.substring(0,p);
     			if (tmp2.equals("Category")) { // check 
-    				
-    				
-    				tmp = tmp.substring(p+1,tmp.length());
-    				System.out.println("start:"+start);
-    	    		System.out.println("end:"+end);
-    	    		System.out.println(tmp);
+ 				tmp = tmp.substring(p+1,tmp.length());
     				while (tmp.indexOf("|")>0) {   // Category may be split by |
     					int mid = tmp.indexOf("|");
     					tmp2 = tmp.substring(0,mid);
@@ -93,12 +101,12 @@ public class MarkDownParser {
     	while (txt.indexOf("]]")>0) {    // Link's sign is [[ and ]] but don't have :
     		int end = txt.indexOf("]]");
     		int start = end;
-    		while ((!txt.substring(start,start+2).equals("[[")) && start>=0)	start--; 
-    	
+    		while ((!txt.substring(start,start+2).equals("[[")) && start>0)	start--; 
+    		if (start ==0 && !txt.substring(start,start+2).equals("[[")) { // [[ miss
+    			txt =txt.substring(0, end)+txt.substring(end+2,txt.length());
+    			continue;			
+    		}
     		tmp = txt.substring(start+2,end);
-    	/*	System.out.println("start:"+start);
-    		System.out.println("end:"+end);
-    		System.out.println(tmp);*/
     		
     		if (tmp.indexOf(":")<0)  {    // [[ : ]] is category
     			while (tmp.indexOf("|")>0) {   // Link may be split by |
@@ -114,6 +122,23 @@ public class MarkDownParser {
     	return Link;
     }
     
+	public String removeHtmlForm(String txt) {
+		while (txt.indexOf("</ref>")>0) {     
+			int end = txt.indexOf("</ref>");
+			int start = end;
+    		while ((!txt.substring(start,start+5).equals("<ref>")) && start>0)	start--;
+    	    txt = txt.substring(0,start)+txt.substring(end+6,txt.length());
+		}  
+		while (txt.indexOf("</ref>")>0) {     
+			int end = txt.indexOf("</ref>");
+			int start = end;
+    		while ((!txt.substring(start,start+5).equals("<ref>")) && start>0)	start--;
+    	    txt = txt.substring(0,start)+txt.substring(end+6,txt.length());
+		}  
+		return txt;
+			
+	}
+	
     public String getCleanTxt(String txt) {
     	while (txt.indexOf("[[")>0) {
     		int p = txt.indexOf("[[");
@@ -136,9 +161,23 @@ public class MarkDownParser {
     		txt =txt.substring(0,p) + txt.substring(p+2, txt.length());
     	}
     	while (txt.indexOf("''")>0) {
-    		int p = txt.indexOf("==");
+    		int p = txt.indexOf("''");
     		txt =txt.substring(0,p) + txt.substring(p+2, txt.length());
     	}
+    	
+    	while (txt.indexOf("</ref>")>0) {     
+			int end = txt.indexOf("</ref>");
+			int start = end;
+    		while ((!txt.substring(start,start+5).equals("<ref>")) && start>0)	start--;
+    	    txt = txt.substring(0,start)+txt.substring(end+6,txt.length());
+		}  
+		while (txt.indexOf(">")>0) {     
+			int end = txt.indexOf(">");
+			int start = end;
+    		while ((!txt.substring(start,start+1).equals("<")) && start>0)	start--;
+    	    txt = txt.substring(0,start)+txt.substring(end+1,txt.length());
+		}  
+    	
     	return txt;
     	
     }
